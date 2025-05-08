d253132a688edef14c5add81d5537d82d729c29c
Pankaj-Sarkar <pankaj.sarkar.dev@gmail.com>
2025-05-08 16:50:36 +0530

    solved reversed integer

---
+++ math/7.reverse_integer.ts
@@ -0,0 +1,15 @@
+function reverse(x: number): number {
+  let isNegative: boolean = false;
+  if (x < 0) {
+      isNegative = true;
+      x = x * -1;
+  }
+  
+  let rev: number = 0;
+  while (x > 0) {
+      const tail = x % 10;
+      rev = rev * 10 + tail;
+      x = Math.floor(x/10);
+  }
+  return isNegative ? -rev : rev;
+};
